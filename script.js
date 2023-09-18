3ba69a058d2f8838ef2da3bb4f40148ad3d531ec
Vikram Reddy <144660004+vikramreddy153@users.noreply.github.com>
2023-09-18 23:57:29 +0530

    third commit

--- script.js
+++ script.js
@@ -33,6 +33,21 @@ function setTheme() {
     setDarkMode();
   }
 }
+function updateDigitalTime() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('digital-time').textContent = timeString;
+}
+
+// Update the digital time every second
+setInterval(updateDigitalTime, 1000);
+
+// Call it once to initialize the time
+updateDigitalTime();
+
 
 function setDarkMode() {
   document.body.setAttribute("theme", "dark");
